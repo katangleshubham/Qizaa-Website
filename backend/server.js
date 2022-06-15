caba87e23d12c4af2d46c1d52859f0831604014c
Abhisheks-Workspace <abhishekjps168@gmail.com>
2022-06-16 01:44:58 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+
+//Config
+
+dotenv.config({path:"backend/config/config.env"});
+
+//Connecting to database
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost: ${process.env.PORT}`)
+})
